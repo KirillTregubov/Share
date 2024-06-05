43ee7d52e32f007bdfdbe92b4b3de0b1b77876bb
Kirill Tregubov <contact@kirilltregubov.com>
2024-06-04 23:12:07 -0400

    changes

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,10 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
+console.log('Server started on port 3000');
+
 wss.on('connection', function connection(ws) {
+  console.log('connected', ws);
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
